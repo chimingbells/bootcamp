246eff107076fd3bdd5f60c49857873e78159ef6
Jae Han <jae.han@ibm.com>
2022-04-14 20:59:05 -0500

    Added main.py script and todo/comments for executing code

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from functions import *
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    """
+    Application entry point
+    """
+    # TODO(chimingbells) Run your code here.
+
+    # Read data from the api
+    # grab_population_data(url)
+
+    # Write data to a filename (csv)
+    # save_json_to_csv(data, filename)
+
+    # Read data back as a dataframe using pandas
+    # read_csv_to_dataframe
+
+    # Write the data to a filename as a dataframe using pandas.
+    # save_dataframe_to_csv
+
+    return
 
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
