d46fe1cbd052d680ea0bc98a028cf1c693fd8a25
Jae Han <jae.han@ibm.com>
2022-04-14 20:58:44 -0500

    Added example template / todos for abstracting code into functions

--- functions.py
+++ functions.py
@@ -1,16 +1,60 @@
 import requests
 import pandas as pd
 
-data = requests.get("https://datausa.io/api/data?drilldowns=Nation&measures=Population")
-datausa = data.text
 
-for key in datausa:
-    print(key, end=" ")
+def grab_population_data(url):
+    """
+    This function calls the datausa api endpoint, and returns the response as a dictionary.
 
-with open("C:\\Users\\13173\\PycharmProjects\\bootcamp\\practice.txt", "w") as f:
-    mystringdict = repr(datausa)
-    f.write(mystringdict)
-    f.close()
+    :param url: url is a str representing an api endpoint from datausa
+    :return: response: dict containing api response from the given api
+    """
 
-csvfile = pd.read_csv (r'C:\Users\13173\PycharmProjects\bootcamp\practice.txt')
-csvfile.to_csv ('C:\\Users\\13173\\PycharmProjects\\bootcamp\\practice.csv', index=None )
+    """
+       
+    hint: 
+        resp = request.get(url)
+        data = resp.json()
+        return data
+    """
+    # TODO(chimingbells) Write Code Below
+    pass
+
+
+def save_json_to_csv(data, filename):
+    """
+    Saves a file to csv locally using the given filename.
+    :param data: dict object containing data from api endpoint
+    :return: None
+    """
+    """
+    hint:
+        Don't use repr, instead use the json.dumps() library (you will also need to use str(data)) 
+    """
+    # TODO(chimingbells) Write Code Below
+    pass
+
+
+def save_dataframe_to_csv(df, filename):
+    """
+    Saves a pandas dataframe to a csv file locally given the filename.
+    :param df: pandas dataframe containing data
+    :param filename:  name of the file you want to save the data to.
+    :return: None
+    """
+    # TODO(chimingbells) Write Code Below
+    pass
+
+
+def read_csv_to_dataframe(filename):
+    """
+    Reads a csv file and returns a pandas dataframe.
+    :param filename: str represnting csv file to laod.
+    :return: df: dataframe containing data in the csv file
+    """
+    # TODO(chimingbells) Write Code Below
+    pass
+
+
+if __name__=='__main__':
+    print('hi')
